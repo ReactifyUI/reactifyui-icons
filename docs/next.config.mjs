113e8e1508578ae3e084a346debff4752bb1679b
prakashaditya13 <prakashaditya13011999+git@gmail.com>
2026-06-22 00:55:51 +0530

    Docs deployment

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+  basePath: isProd ? "/reactifyui-icons" : "",
+  assetPrefix: isProd ? "/reactifyui-icons/" : "",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
